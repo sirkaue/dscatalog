0dbcf0f091cbb2bc9a7f21cc3547702a037ab0ce
sirkaue <kauekdslopes@gmail.com>
2024-05-29 21:17:15 -0300

    CategoryResource

---
+++ src/main/java/com/sirkaue/dscatalog/entities/Category.java
@@ -0,0 +1,45 @@
+package com.sirkaue.dscatalog.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable {
+    private Long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category category)) return false;
+        return Objects.equals(getId(), category.getId()) && Objects.equals(getName(), category.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
+}
