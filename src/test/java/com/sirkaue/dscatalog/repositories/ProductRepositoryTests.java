d65ad62194cada42f595d428bb603fc5ad92a994
sirkaue <kauekdslopes@gmail.com>
2024-06-14 18:36:06 -0300

    Delete tests

---
+++ src/test/java/com/sirkaue/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,42 @@
+package com.sirkaue.dscatalog.repositories;
+
+import com.sirkaue.dscatalog.entities.Product;
+import com.sirkaue.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.doThrow;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @MockBean
+    private ProductRepository mockRepository;
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists() {
+        long existingId = 1L;
+
+        repository.deleteById(existingId);
+        Optional<Product> result = repository.findById(existingId);
+        Assertions.assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void deleteShouldResourceNotFoundException() {
+        long nonExistingId = 1000L;
+        doThrow(new ResourceNotFoundException("Resource not found")).when(mockRepository).deleteById(nonExistingId);
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            mockRepository.deleteById(nonExistingId);
+        });
+    }
+}
