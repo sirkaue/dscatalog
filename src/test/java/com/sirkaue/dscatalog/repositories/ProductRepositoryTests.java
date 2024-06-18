982fad59312ae75cf24c428fb669357e219a8f88
sirkaue <kauekdslopes@gmail.com>
2024-06-18 13:43:11 -0300

    ProductRepositoryTests

--- src/test/java/com/sirkaue/dscatalog/repositories/ProductRepositoryTests.java
+++ src/test/java/com/sirkaue/dscatalog/repositories/ProductRepositoryTests.java
@@ -1,17 +1,14 @@
 package com.sirkaue.dscatalog.repositories;
 
 import com.sirkaue.dscatalog.entities.Product;
-import com.sirkaue.dscatalog.services.exceptions.ResourceNotFoundException;
+import com.sirkaue.dscatalog.utils.Factory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Optional;
-
-import static org.mockito.Mockito.doThrow;
 
 @DataJpaTest
 public class ProductRepositoryTests {
@@ -19,31 +16,43 @@ public class ProductRepositoryTests {
     @Autowired
     private ProductRepository repository;
 
-    @MockBean
-    private ProductRepository mockRepository;
-
     private long existingId;
     private long nonExistingId;
+    private long countTotalProducts;
 
     @BeforeEach
     void setUp() {
         existingId = 1L;
         nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void saveShouldPersistWithAutoIncrementWhenIdIsNull() {
+        Product product = Factory.createProduct();
+        product.setId(null);
+        product = repository.save(product);
+
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProducts + 1, product.getId());
     }
 
     @Test
     public void deleteShouldDeleteObjectWhenIdExists() {
         repository.deleteById(existingId);
         Optional<Product> result = repository.findById(existingId);
-        Assertions.assertFalse(result.isPresent());
+        Assertions.assertTrue(result.isEmpty());
     }
 
     @Test
-    public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
-        doThrow(new ResourceNotFoundException("Resource not found")).when(mockRepository).deleteById(nonExistingId);
+    public void findByIdShouldReturnExistingOptionalProductWhenIdExists() {
+        Optional<Product> product = repository.findById(existingId);
+        Assertions.assertTrue(product.isPresent());
+    }
 
-        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
-            mockRepository.deleteById(nonExistingId);
-        });
+    @Test
+    public void findByIdShouldReturnEmptyOptionalProductWhenIdDoesNotExists() {
+        Optional<Product> product = repository.findById(nonExistingId);
+        Assertions.assertTrue(product.isEmpty());
     }
 }
