24c731bf05f8eb9de7a5ffaafa830c52f3366c3d
sirkaue <kauekdslopes@gmail.com>
2024-07-25 18:56:13 -0300

    mapeamentos objeto relacional, seed do banco

--- src/main/java/com/sirkaue/dscatalog/entities/Role.java
+++ src/main/java/com/sirkaue/dscatalog/entities/Role.java
@@ -1,11 +1,18 @@
 package com.sirkaue.dscatalog.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_role")
 public class Role implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String authority;
 
     public Role() {
--- src/main/java/com/sirkaue/dscatalog/entities/User.java
+++ src/main/java/com/sirkaue/dscatalog/entities/User.java
@@ -1,18 +1,28 @@
 package com.sirkaue.dscatalog.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
     private String email;
     private String password;
 
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
     public User() {
