509d8f1809214e2bbc3777a0db6d84d134c40f8c
sirkaue <kauekdslopes@gmail.com>
2024-07-25 21:58:12 -0300

    controlador, email unique

---
+++ src/main/java/com/sirkaue/dscatalog/resources/UserResource.java
@@ -0,0 +1,57 @@
+package com.sirkaue.dscatalog.resources;
+
+import com.sirkaue.dscatalog.dto.UserDto;
+import com.sirkaue.dscatalog.dto.UserInsertDto;
+import com.sirkaue.dscatalog.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<Page<UserDto>> findAll(Pageable pageable) {
+        Page<UserDto> list = service.findAllPaged(pageable);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> findById(@PathVariable Long id) {
+        UserDto dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDto> insert(@RequestBody UserInsertDto dto) {
+        UserDto newDto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newDto.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(newDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> update(@PathVariable Long id, @RequestBody UserDto dto) {
+        UserDto validatedDto = service.update(id, dto);
+        return ResponseEntity.ok().body(validatedDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
